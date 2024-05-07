15ddba573222dc88b13e52f68391ee3ed1e81cba
Documenter.jl <documenter@juliadocs.github.io>
2024-05-07 20:19:13 +0000

    build based on ddcfb91

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MicroscopeControl","category":"page"},{"location":"#MicroscopeControl","page":"Home","title":"MicroscopeControl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MicroscopeControl.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MicroscopeControl]","category":"page"}]
+}
