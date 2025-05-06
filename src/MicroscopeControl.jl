dcebe43b5acf77c8bda5ce246d3ecb6d1f5ecb8f
Ali Kazemi <ali.kazemins@gmail.com>
2025-05-05 23:34:20 -0600

    Fix typo in export statement for hardware modules in MicroscopeControl.jl

--- src/MicroscopeControl.jl
+++ src/MicroscopeControl.jl
@@ -30,6 +30,11 @@ using .HardwareImplementations.CrystaLaserControl
 using .HardwareImplementations.VortranLaserControl
 using .HardwareImplementations.OK_XEM
 
+# Exportimg all modules
+export DCAM4, SimulatedCamera, SimulatedStage, PI, MadCityLabs, PI_N472, ThorCamCSC
+export SimulatedLight, NIDAQcard, TCubeLaserControl, TransmissionDaqControl
+export CrystaLaserControl, VortranLaserControl, OK_XEM
+
 # Export h5 file saving methods
 export save_h5, save_attributes_and_data
 
