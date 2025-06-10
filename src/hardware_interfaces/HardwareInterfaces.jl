b60a920278500cab3e86053652e46b2f7b0bd12a
ellyse-taylor <84996743+ellyse-taylor@users.noreply.github.com>
2025-06-10 16:57:27 -0600

    include missing files

--- src/hardware_interfaces/HardwareInterfaces.jl
+++ src/hardware_interfaces/HardwareInterfaces.jl
@@ -15,4 +15,6 @@ include("stage_interface/StageInterface.jl")
 
 include("lightsource_interface/LightSourceInterface.jl")
 include("daq_interface/DAQInterface.jl")
+include("scanner_interface/ScannerInterface.jl")
+include("triggerscope_interface/TrigInterface.jl")
 end
