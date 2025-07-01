b6f521605fe9aee010426d0e19da4e9e81a78226
AliKNS <ali.kazemins@gmail.com>
2025-07-01 17:41:12 -0600

    create testing file for characterizing the beam, track the intensity on a 3D graph, and find the center

---
+++ dev/beam_characterization.jl
@@ -0,0 +1,81 @@
+using Revise
+using GLMakie, ImageFiltering
+using MicroscopeControl
+using MicroscopeControl.HardwareImplementations.ThorCamDCx
+using Statistics
+function live_view_3d(
+    camera::ThorcamDCXCamera, 
+    framerate::Float64 = 15.0, 
+    exposure_time::Float64 = 0.01,
+)
+    initialize(camera)
+    camera.exposure_time = exposure_time
+    live(camera)
+    start = time()
+    initial_frame = getlastframe(camera)
+    fig = Figure(resolution = (1200, 500))
+    ax2d = Axis(fig[1, 1], title = "Live Camera Feed")
+    ax3d = Axis3(fig[1, 2], title = "3D Intensity Map")
+    
+    frame_obs = Observable(initial_frame)
+    heatmap!(ax2d, frame_obs, colormap = :inferno)
+
+    nx, ny = size(initial_frame)
+    x = 1:nx
+    y = 1:ny
+
+    surface!(ax3d, x, y, frame_obs; colormap = :viridis)
+
+    display(fig)
+    window_closer(fig, () -> shutdown(camera))
+    cx = Observable(0.0)
+    cy = Observable(0.0)
+    @async begin
+        while camera.is_running == 1
+            frame = getlastframe(camera)
+            
+            if frame !== nothing
+                frame_obs[] = frame
+                duration = round(time() - start, digits = 2)
+                ax2d.title = "Live Camera Feed - Time: $duration seconds"
+                cx[], cy[] = bright_median(frame)
+                scatter!(ax2d, cx, cy, color=:teal, markersize=10)
+            end
+            sleep(1/framerate)
+        end
+    end
+    return fig, ax2d, ax3d, frame_obs
+end
+
+function window_closer(fig, cleanups...)
+    on(events(fig).window_open) do is_open
+        if !is_open
+            println("Window closed - running cleanup...")
+            for cleanup in cleanups
+                try
+                    cleanup()
+                catch e
+                    println("Error during cleanup: $e")
+                end
+            end
+            println("Cleanup complete!")
+        end
+    end
+end
+
+function bright_median(frame; frac=0.05)
+    thresh = quantile(vec(frame), 1 - frac)  # threshold for top frac
+    inds = findall(>=(thresh), frame)
+    xs = []; ys = []
+    for index in inds
+        push!(xs, index[1])
+        push!(ys, index[2])
+    end
+    center_x = median(xs)
+    center_y = median(ys)
+    return center_x, center_y
+end
+
+camera = ThorcamDCXCamera()
+live_view_3d(camera)
+shutdown(camera)
